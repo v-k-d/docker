26b4a40d5725bec29b45d97ade5bcd603c363000
Victor Karam Damasio <victorkaramdiru@gmail.com>
2023-10-18 21:22:35 -0300

    29. Passing Environment Variables To Docker Container

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test{
+    public static void main(String[] args){
+        System.out.println("Received # is " + args[0]);
+        int number = Integer.parseInt(args[0]);
+        //fing squares
+        for(int i = 1; i <= number; i++){
+            System.out.println("Square of " + i + " is --> " + (i * i));
+        }
+    }
+}
